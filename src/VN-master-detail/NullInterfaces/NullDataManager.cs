eb18c2016b88556edceeeb3e6efe0791cf95c5c1
ShrayzzDev <renaudflash@gmail.com>
2024-11-18 12:08:54 +0100

    [ADD] NullDataManager (does nothing)

---
+++ src/VN-master-detail/NullInterfaces/NullDataManager.cs
@@ -0,0 +1,31 @@
+﻿using DTO;
+using Interfaces;
+using Model.Novel;
+
+namespace NullInterfaces
+{
+    /// <summary>
+    /// Implements IDataManager but does nothing.
+    /// Used for default initializations.
+    /// </summary>
+    public class NullDataManager : IDataManager
+    {
+        /// <inheritdoc/>
+        public Task<DetailedNovel?> GetDetailedNovelById(string id)
+        {
+            return Task.FromResult((DetailedNovel?)null);
+        }
+
+        /// <inheritdoc/>
+        public Task<BasicNovel?> GetNovelById(string id)
+        {
+            return Task.FromResult((BasicNovel?)null);
+        }
+
+        /// <inheritdoc/>
+        public Task<IEnumerable<BasicNovel>> GetNovels(int index, int count, Criteria criteria)
+        {
+            return Task.FromResult((IEnumerable<BasicNovel>)[]);
+        }
+    }
+}
