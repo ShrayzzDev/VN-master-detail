1410f8fe41f1d4eb66202a545186bff6b52fc3d2
ShrayzzDev <renaudflash@gmail.com>
2024-11-18 15:17:24 +0100

    [DEL] DataManager from App

--- src/VN-master-detail/VN-master-detail/App.xaml.cs
+++ src/VN-master-detail/VN-master-detail/App.xaml.cs
@@ -5,7 +5,6 @@ namespace VN_master_detail
 {
     public partial class App : Application
     {
-        public readonly IDataManager DataManager = new DataManager(new NovelStub());
         public App()
         {
             InitializeComponent();
