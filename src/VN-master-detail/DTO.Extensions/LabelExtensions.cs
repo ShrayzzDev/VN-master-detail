422eea8036453048818c73b9bbc3a80ab723fb06
ShrayzzDev <renauddflash@gmail.com>
2024-11-26 11:03:42 +0100

    [ADD] DTO/Model Label

---
+++ src/VN-master-detail/DTO.Extensions/LabelExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Extensions
+{
+    public static class LabelExtensions
+    {
+        public static Model.Label ToModel(this LabelDTO dto)
+        {
+            return new Model.Label(
+                dto.id,
+                dto.name
+            );
+        }
+    }
+}
